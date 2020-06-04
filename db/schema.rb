d47c117c8ac1ff08f8445bea6e4b8202179bcab4
Gracie <m.graciemcguire@gmail.com>
2020-06-04 13:11:21 -0600

    avoided a catastophe phew

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_181813) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
