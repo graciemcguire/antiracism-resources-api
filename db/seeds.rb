89fc98ad43c5dc98e0b42c03d431c6ba1263903b
luanesouza <luanesouza411@gmail.com>
2020-06-10 15:32:00 -0400

    first round of data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,58 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all()
+Resource.destroy_all()
+Vote.destroy_all()
+
+p "🔥 all destroyed 🔥"
+
 user1 = User.create(email: 'whitney@email.com', password: 'aishuia', admin: false )
 
 # Resource.create(title: 'A new resource', added_resource_id: user1.id)
-resource1 = Resource.create(title: 'This is a test')
+resource1 = Resource.create([
+  {
+    kind: 'book',
+    approved: true,
+    url: 'https://www.commonsensemedia.org/lists/coretta-scott-king-book-award-winners',
+    description: '',
+    title: 'Coretta Scott King Book Award Winners: books for children and young adults',
+    twitter_link: '',
+    insta_link: '',
+    facebook_link: ''
+  },
+  {
+    kind: 'podcast',
+    approved: true,
+    url: 'https://podcasts.apple.com/us/podcast/parenting-forward/id1403686245?i=1000474951309',
+    description:'',
+    title: 'Parenting Forward podcast episode ‘Five Pandemic Parenting Lessons with Cindy Wang Brandt’',
+    twitter_link: '',
+    insta_link: '',
+    facebook_link: ''
+  },
+  {
+    kind: 'article',
+    approved: true,
+    url: 'PBS’s Teaching Your Child About Black History Month',
+    description: '',
+    title: '',
+    twitter_link: '',
+    insta_link: '',
+    facebook_link: ''
+  },
+  {
+    kind: 'article',
+    approved: true,
+    url: 'https://www.theatlantic.com/ideas/archive/2020/05/americas-racial-contract-showing/611389/',
+    description: '',
+    title: '“America’s Racial Contract Is Killing Us” by Adam Serwer | Atlantic (May 8, 2020)',
+    twitter_link: '',
+    insta_link: '',
+    facebook_link: ''
+  }
+  ])
 
-Vote.create(user_id: user1.id, resource_id: resource1.id, vote: 'upvote')
+# Vote.create(user_id: user1.id, resource_id: resource1.id, vote: 'upvote')
 
-puts 'done'
+puts 'all reborn'
